4d799de100e228252717ff4e1b6e89456f566920
lishuang <1729713807@qq.com>
2020-07-12 20:14:55 +0800

    add RandomTest

--- src/com/iweb/study/day06/AndTest.java
+++ src/com/iweb/study/day06/AndTest.java
@@ -1,5 +1,8 @@
 package com.iweb.study.day06;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @Author Moro
  * @Date 2020/7/10 10:35
@@ -7,6 +10,9 @@ package com.iweb.study.day06;
  */
 public class AndTest {
     public static void main(String[] args) {
-
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+        String format = sdf.format(date);
+        System.out.println(format);
     }
 }
